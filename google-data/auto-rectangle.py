02a714e15692a5e88128b41c28978d1a62b9e559
mruescher <mruescher@isa-hamburg.com>
2018-12-26 20:41:23 +0100

    added arguments to script and added first image modifiy level

--- google-data/auto-rectangle.py
+++ google-data/auto-rectangle.py
@@ -1,16 +1,30 @@
 import numpy as np
-import cv2 as cv
+import cv2
 import glob
 import imutils
 import re
 import os
+import argparse
 
-savingType = 'voc-xml'  # 'voc-xml' or 'csv-with-array-keys' or 'csv-with-object-name' or 'Cartucho/mAP'
-xmlFilePathPrefix = '/../data/'
+parser = argparse.ArgumentParser()
+parser.add_argument('--savingType', default='voc-xml',
+                    choices=['voc-xml', 'csv-with-array-keys', 'csv-with-object-name', 'Cartucho/mAP'],
+                    help='saving type of the bounding boxes (default: %(default)s)')
+parser.add_argument('--xmlPath', default='/../data/', help='path where the xmls will be saved (default: %(default)s)')
+parser.add_argument('--imageModifyLevel', default='0',
+                    help='how much the images will be modified (default: %(default)s)')
+parser.add_argument('--imagePath', default='/../data/_main/*/*.jpg',
+                    help='relative glob path to the image direcoty (default: %(default)s)')
+
+args = parser.parse_args()
+savingType = args.savingType
+xmlFilePathPrefix = args.xmlPath
+imageModifyLevel = args.imageModifyLevel
+imagePath = args.imagePath
 
 # Read image data
 dir_path = os.path.dirname(os.path.realpath(__file__))
-images = sorted(glob.glob(dir_path + '/../data/*/*/*.jpg'))
+images = sorted(glob.glob(dir_path + imagePath))
 dataDirectories = sorted(glob.glob(dir_path + '/*/'))
 dataFiles = {}
 
@@ -29,69 +43,68 @@ for dirName in dataDirectories:
 i = 1
 for imgNamePath in images:
     # print(imgNamePath)
-    img = cv.imread(imgNamePath)
+    img = cv2.imread(imgNamePath)
 
     imgNamePath = imgNamePath.replace(dir_path + '/', '', 1)
 
-    regex = r"^(.*?)\/(.*?)\/_main/.*?/(.*)\.jpg$"
+    regex = r"^../data/_main/(.*?)(/)(.*)\.jpg$"
 
     dataDir = re.findall(regex, imgNamePath)[0][0]
     objectType = re.findall(regex, imgNamePath)[0][1]
     imgName = re.findall(regex, imgNamePath)[0][2]
 
     # Adding white border, because some images are to big
-    # img = cv.copyMakeBorder(img, 50, 50, 50, 50, cv.BORDER_CONSTANT, value=[255, 255, 255])
-
-    # Define colors and threshold for contours
-    # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    # white_bg = 255 * np.ones_like(img)
-    # ret, thresh = cv.threshold(gray, 60, 255, cv.THRESH_BINARY_INV)
-    # blur = cv.medianBlur(thresh, 1)
-    # kernel = np.ones((10, 20), np.uint8)
+    # img = cv2.copyMakeBorder(img, 50, 50, 50, 50, cv2.BORDER_CONSTANT, value=[255, 255, 255])
 
     imgOrg = img.copy()
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    gray = cv.bilateralFilter(gray, 11, 17, 17)
-    img = cv.Canny(gray, 30, 200)
-    # ret, img = cv.threshold(img, 60, 255, cv.THRESH_BINARY_INV)
-    ret, img = cv.threshold(img, 0, 255, 0)
+    imgOrg2 = img.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    img = cv2.Canny(gray, 30, 200)
+    # ret, img = cv2.threshold(img, 60, 255, cv2.THRESH_BINARY_INV)
+    ret, img = cv2.threshold(img, 0, 255, 0)
 
-    # imgOrg2 = img.copy()
-    # src = cv.imread(imgOrg2)
-    # tmp = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-    # _, alpha = cv.threshold(tmp, 0, 255, cv.THRESH_BINARY)
-    # b, g, r = cv.split(src)
-    # rgba = [b, g, r, alpha]
-    # dst = cv.merge(rgba, 4)
-    # cv.imshow(imgOrg2, img)
-
-    # Use OpenCv for finding contours
-    # img_dilation = cv.dilate(blur, kernel, iterations=1)
-    # im2, ctrs, hier = cv.findContours(img.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-    # ret, thresh = cv.threshold(img, 60, 255, cv.THRESH_BINARY_INV)
-    #
     ## sort the containers
-    img = cv.convertScaleAbs(img)
-    ctrs = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    img = cv2.convertScaleAbs(img)
+    ctrs = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     ctrs = imutils.grab_contours(ctrs)
-    ctrs = sorted(ctrs, key=cv.contourArea, reverse=True)[:10]
-
-    # thresh, img = cv.threshold(img, 128, 255, 0)
-    #
-    # ctrs = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    # ctrs = imutils.grab_contours(ctrs)
-    # ctrs = sorted(ctrs, key=cv.contourArea, reverse=True)[:10]
-
-    # cv.drawContours(image, contours, -1, (0, 255, 0), 3)
+    ctrs = sorted(ctrs, key=cv2.contourArea, reverse=True)[:10]
 
     imgHeight, imgWidth = img.shape
 
-    # cv.imshow('Object detector', edged)
+    if (imageModifyLevel == '2'):
+        img = cv2.cvtColor(imgOrg, cv2.COLOR_BGR2RGB)
+        lower_white = np.array([250, 250, 250], dtype=np.uint8)
+        upper_white = np.array([255, 255, 255], dtype=np.uint8)
+        mask = cv2.inRange(img, lower_white, upper_white)  # could also use threshold
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (
+            3, 3)))  # "erase" the small white points in the resulting mask
+        mask = cv2.bitwise_not(mask)  # invert mask
+
+        # load background (could be an image too)
+        bk = np.full(img.shape, (66, 128, 200), dtype=np.uint8)  # white bk
+
+        # get masked foreground
+        fg_masked = cv2.bitwise_and(img, img, mask=mask)
+
+#24 10 15 16
+        # fg_masked = np.zeros((400, 400, 3), dtype="uint8")
+        # fg_masked[np.where((fg_masked == [0,0,0]).all(axis=2))] = [66, 244, 104]
+
+        # cv2.imshow(imgName, fg_masked)
+        # get masked background, mask must be inverted
+        mask = cv2.bitwise_not(mask)
+        bk_masked = cv2.bitwise_and(bk, bk, mask=mask)
+        # cv2.imshow(imgName, bk_masked)
+
+        # combine masked foreground and masked background
+        final = cv2.bitwise_or(fg_masked, bk_masked)
+        mask = cv2.bitwise_not(mask)  # revert mask to original
+        cv2.imshow(imgName, final)
 
     # Foreach container found
     for cnt in ctrs:
-        x, y, w, h = cv.boundingRect(cnt)
+        x, y, w, h = cv2.boundingRect(cnt)
         w = x + w
         h = h + y
 
@@ -135,8 +148,7 @@ for imgNamePath in images:
         w = w - x
         h = h - y
 
-        cv.rectangle(imgOrg, (x, y), (x + w, y + h), (66, 244, 104), 2)
-        # cv.rectangle(img, (x, y), (x + w, y + h), Scalar(0, 255, 128), 2)
+        cv2.rectangle(imgOrg, (x, y), (x + w, y + h), (66, 244, 104), 2)
         if (savingType == 'csv-with-array-keys' or savingType == 'csv-with-object-name'):
             csvStr = "%s;%s;%s;%s;%s;%s;%s;%s\n" % (imgNamePath, w, h, objectType, x, y, x + w, y + h)
             if (savingType == 'csv-with-array-keys'):
@@ -156,33 +168,32 @@ for imgNamePath in images:
                 objectType = 'valve'
             if (int(imgName) > 40):
                 objectType = 'handwheel'
-            xmlString = """ 
-            <annotation>
-                <folder>{objectType}</folder>
-                <filename>{imgNamePath}</filename>
-                <path>{imgNamePath}</path>
-                <source>
-                    <database>Unknown</database>
-                </source>
-                <size>
-                    <width>{imgHeight}</width>
-                    <height>{imgHeight}</height>
-                    <depth>3</depth>
-                </size>
-                <segmented>0</segmented>
-                <object>
-                    <name>{objectType}</name>
-                    <pose>Unspecified</pose>
-                    <truncated>0</truncated>
-                    <difficult>0</difficult>
-                    <bndbox>
-                        <xmin>{xmin}</xmin>
-                        <ymin>{ymin}</ymin>
-                        <xmax>{xmax}</xmax>
-                        <ymax>{ymax}</ymax>
-                    </bndbox>
-                </object>
-            </annotation>
+            xmlString = """<annotation>
+    <folder>{objectType}</folder>
+    <filename>{imgNamePath}</filename>
+    <path>{imgNamePath}</path>
+    <source>
+        <database>Unknown</database>
+    </source>
+    <size>
+        <width>{imgHeight}</width>
+        <height>{imgHeight}</height>
+        <depth>3</depth>
+    </size>
+    <segmented>0</segmented>
+    <object>
+        <name>{objectType}</name>
+        <pose>Unspecified</pose>
+        <truncated>0</truncated>
+        <difficult>0</difficult>
+        <bndbox>
+            <xmin>{xmin}</xmin>
+            <ymin>{ymin}</ymin>
+            <xmax>{xmax}</xmax>
+            <ymax>{ymax}</ymax>
+        </bndbox>
+    </object>
+</annotation>
             """.format(
                 imgNamePath=imgName + '.jpg',
                 xmin=x,
@@ -193,15 +204,24 @@ for imgNamePath in images:
                 imgHeight=imgHeight,
                 imgWidth=imgWidth
             )
-            path = dir_path + xmlFilePathPrefix + dataDir + '/' + objectType
+            path = dir_path + xmlFilePathPrefix + dataDir + '/'
+            cv2.imwrite(path + '/' + imgName + '.jpg', imgOrg)
             os.makedirs(path, exist_ok=True)
             file = open(path + '/' + imgName + '.xml', 'w')
             file.write(xmlString)
             file.close()
 
         if (savingType == 'Cartucho/mAP'):
+            if (int(imgName) <= 20):
+                objectType = 'gauge'
+            if (int(imgName) > 20 <= 40):
+                objectType = 'valve'
+            if (int(imgName) > 40):
+                objectType = 'handwheel'
             txtStr = "%s %s %s %s %s\n" % (objectType, x, y, x + w, y + h)
-            file = open(dir_path + '/../github-repos/mAP/ground-truth/' + imgName + '.txt', 'w')
+            path = dir_path + xmlFilePathPrefix + dataDir + '/'
+            file = open(path + '/' + imgName + '.txt', 'w')
+            # file = open(dir_path + '/../github-repos/mAP/ground-truth/' + imgName + '.txt', 'w')
             file.write(txtStr)
             file.close()
 
@@ -209,26 +229,12 @@ for imgNamePath in images:
         break
 
     # Show the image with a python window
-    cv.imshow(imgName, imgOrg)
+    # cv2.imshow(imgName, imgOrg2)
 
     # # Press any key to close the image
-    cv.waitKey(0)
+    cv2.waitKey(0)
     #
     # # Clean up
-    cv.destroyAllWindows()
+    cv2.destroyAllWindows()
     print('Verarbeitet: ' + ("%.2f" % (i / len(images) * 100) + '%'))
     i = i + 1
-
-# filename,width,height,class,xmin,ymin,xmax,ymax
-# tong-line-tong-torque.png,400,400,gauge,28,146,180,289
-
-# https://github.com/qqwweee/keras-yolo3
-# x_min,y_min,x_max,y_max,class_id
-
-# [x,y,z,w]
-# [0,1,2,3] Dimensionen
-
-
-# ValueError: Dimension 0 in both shapes must be equal, but are 1 and 75. Shapes are
-# [1,1,1024,39] and [75,1024,1,1]. for 'Assign_360' (op: 'Assign') with input shapes: [1,1,1024,39], [75,1024,1,1].
-# (tensorflow1) Marlons-MacBook-Pro-2:keras-yolo3-mr mruescher$
