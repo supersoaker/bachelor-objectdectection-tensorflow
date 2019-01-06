60c4cb7a80f8fd21ecbe7091c11fa235eda8a47e
mruescher <mruescher@isa-hamburg.com>
2019-01-06 15:12:32 +0100

    fixed optimize bug

--- google-data/get-best-thresh-val-with-map.py
+++ google-data/get-best-thresh-val-with-map.py
@@ -5,8 +5,8 @@ i = 0.15
 while (i < 0.2):
     subprocess.run([
         './flow --imgdir ./bachelor-objectdectection-tensorflow/data/probe2/test/ --model cfg/tiny-yolo-ipm.cfg --load 255 --gpu 1.0 --threshold 0.173 --txt'],
-        stdout=subprocess.PIPE)
-    result = subprocess.run(['python mAP/main.py'], stdout=subprocess.PIPE)
+        stdout=subprocess.PIPE, shell=True)
+    result = subprocess.run(['python mAP/main.py'], stdout=subprocess.PIPE, shell=True)
     output = result.stdout.decode('utf-8')
     p = re.findall(r'mAP = (.*)%', output, re.MULTILINE)
     print(p)
