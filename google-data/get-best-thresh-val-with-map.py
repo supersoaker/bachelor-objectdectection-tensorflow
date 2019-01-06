66efee4fbbe4959fbb80cf7c428497fdfdd74797
mruescher <mruescher@isa-hamburg.com>
2019-01-06 15:53:45 +0100

    Removed all unnecessary output from subprocess

--- google-data/get-best-thresh-val-with-map.py
+++ google-data/get-best-thresh-val-with-map.py
@@ -6,7 +6,7 @@ while (i < 0.2):
     print('loading with thresval: ' + str(i))
     subprocess.run([
         './flow --imgdir ./bachelor-objectdectection-tensorflow/data/probe2/test/ --model cfg/tiny-yolo-ipm.cfg --load 255 --gpu 1.0 --threshold ' + str(
-            i) + ' --txt'], stdout=subprocess.PIPE, shell=True)
+            i) + ' --txt'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
     result = subprocess.run(['python mAP/main.py'], stdout=subprocess.PIPE, shell=True)
     output = result.stdout.decode('utf-8')
     p = re.findall(r'mAP = (.*)%', output, re.MULTILINE)
