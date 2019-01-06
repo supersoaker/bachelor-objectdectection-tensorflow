c07f921a4cc8cae8106ed789c954a7e5881c8077
mruescher <mruescher@isa-hamburg.com>
2019-01-06 15:09:21 +0100

    added threshold optimizer

---
+++ google-data/get-best-thresh-val-with-map.py
@@ -0,0 +1,13 @@
+import subprocess
+import re
+
+i = 0.15
+while (i < 0.2):
+    subprocess.run([
+        './flow --imgdir ./bachelor-objectdectection-tensorflow/data/probe2/test/ --model cfg/tiny-yolo-ipm.cfg --load 255 --gpu 1.0 --threshold 0.173 --txt'],
+        stdout=subprocess.PIPE)
+    result = subprocess.run(['python mAP/main.py'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    p = re.findall(r'mAP = (.*)%', output, re.MULTILINE)
+    print(p)
+    i = i + 0.05
