c26151fef57a3e1cb48c8e879c193f8da994828d
mruescher <mruescher@isa-hamburg.com>
2019-01-06 15:22:31 +0100

    fixed bug from optimizer

--- google-data/get-best-thresh-val-with-map.py
+++ google-data/get-best-thresh-val-with-map.py
@@ -3,11 +3,12 @@ import re
 
 i = 0.15
 while (i < 0.2):
+    print('loading with thresval: ' + str(i))
     subprocess.run([
-        './flow --imgdir ./bachelor-objectdectection-tensorflow/data/probe2/test/ --model cfg/tiny-yolo-ipm.cfg --load 255 --gpu 1.0 --threshold 0.173 --txt'],
-        stdout=subprocess.PIPE, shell=True)
+        './flow --imgdir ./bachelor-objectdectection-tensorflow/data/probe2/test/ --model cfg/tiny-yolo-ipm.cfg --load 255 --gpu 1.0 --threshold ' + str(
+            i) + ' --txt'], stdout=subprocess.PIPE, shell=True)
     result = subprocess.run(['python mAP/main.py'], stdout=subprocess.PIPE, shell=True)
     output = result.stdout.decode('utf-8')
     p = re.findall(r'mAP = (.*)%', output, re.MULTILINE)
     print(p)
-    i = i + 0.05
+    i = i + 0.005
